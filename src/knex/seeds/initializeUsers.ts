cba4a90eb8439be1c88ae512fa57e44be0e44813
David Woohyun Lee <david@heybaby.com>
2022-12-12 02:48:02 -0600

    updated seed

--- src/knex/seeds/initializeUsers.ts
+++ src/knex/seeds/initializeUsers.ts
@@ -4,8 +4,26 @@ export async function seed(knex: Knex): Promise<void> {
   await knex('user').del();
 
   await knex('user').insert([
-    { firstName: 'Jefferson', lastName: 'Follmann' },
-    { firstName: 'Andressa', lastName: 'Duarte' },
-    { firstName: 'Antônio', lastName: 'Follmann' },
+    { 
+      id: 1,
+      name: 'Jefferson', 
+      price: 1,
+      userId: 1,
+      active: true,
+    },
+    { 
+      id: 2,
+      name: 'Tom', 
+      price: 2,
+      userId: 2,
+      active: true,
+    },
+    { 
+      id: 3,
+      name: 'Cook', 
+      price: 3,
+      userId: 3,
+      active: true,
+    },
   ]);
 }

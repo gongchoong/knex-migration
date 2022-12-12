ce9a47904ddb520e33411703d96aca444d2da885
David Woohyun Lee <david@heybaby.com>
2022-12-12 02:42:05 -0600

    updated knexfile

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -2,7 +2,10 @@ import Knex from 'knex';
 
 const config: Knex.Config = {
   client: 'pg',
-  connection: process.env.DATABASE_URL,
+  connection: { 
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  },
   migrations: {
     extension: 'ts',
     directory: 'knex/migrations',

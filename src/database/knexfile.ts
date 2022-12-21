ee90bf95b68412c3022bfd1e500d54d739032aa9
David Woohyun Lee <david@heybaby.com>
2022-12-21 11:58:22 -0600

    added ssl

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -18,6 +18,7 @@ module.exports = {
   local: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
     migrations: {
       directory: './migrations',
     },

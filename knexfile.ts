6c460c6a8abd93a84e4fb469e52a43aeddf09b14
David Woohyun Lee <david@heybaby.com>
2022-12-30 23:56:45 -0600

    db setup

--- knexfile.ts
+++ knexfile.ts
@@ -1,19 +1,55 @@
-import Knex from 'knex';
+import Knex from "knex";
 
-const config: Knex.Config = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { knexSnakeCaseMappers } = require('objection');
+const local = 'postgres://postgres:postgres@host.docker.internal:5432/sample_db?sslmode=disable';
+// const production = process.env.DATABASE_URL;
+
+const defaultOptions: Knex.Config = {
   client: 'pg',
-  connection: { 
-    connectionString: process.env.DATABASE_URL,
-    // ssl: { rejectUnauthorized: false }
-  },
-  migrations: {
-    extension: 'ts',
-    directory: './migrations',
-  },
-  seeds: {
-    extension: 'ts',
-    directory: './seeds',
-  },
-};
+    connection: local,
+    ...knexSnakeCaseMappers(),
+    migrations: {
+      directory: './migrations',
+    },
+    seeds: {
+      directory: './seeds',
+    },
+}
 
-export default config;
+const knexConfig: KnexFileEnv = {
+  local: {
+    ...defaultOptions,
+  },
+
+  development: {
+    ...defaultOptions,
+  },
+
+  test: {
+    ...defaultOptions,
+  },
+
+  staging: {
+    ...defaultOptions,
+  },
+
+  production: {
+    ...defaultOptions,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+}
+
+export interface KnexFileEnv {
+  local: Knex.Config
+  development: Knex.Config
+  test: Knex.Config
+  staging: Knex.Config
+  production: Knex.Config
+  [index: string]: Knex.Config
+}
+
+module.exports = knexConfig
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import express from 'express';
 
 const app = express();
 const port = Number(process.env.PORT ?? 8080);
+const dbSetup = require('./db-setup');
+
+dbSetup();
 
 app.get('/', (req, res) => {
-  res.send('Koa Test!!!');
+  res.send('Koa!!!');
 });
 
 app.listen(port, () => console.log(`Express is listening at http://localhost:${port}`));
